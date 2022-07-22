356c75c21c166e9c6301d017de0ad25d027a3f9d
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2022-07-22 14:06:41 +0530

    Time: 2048 ms (5.02%), Space: 658.8 MB (5.02%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,7 +1,46 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) 
+    
+    int help(vector<int>nums,int i , int j , vector<vector<int>>&dp)
     {
-        return true;
+        if(i>j)
+        {
+            return 0;
+        }
+        
+        
+        
+        if(dp[i][j]!=-1)
+        {
+            return dp[i][j];
+        }
+        
+        int pick_first = nums[i] + min(help(nums,i+2,j,dp),help(nums,i+1,j-1,dp));
+        int pick_last = nums[j] + min(help(nums,i+1,j-1,dp),help(nums,i,j-2,dp));
+        
+        return dp[i][j] = max(pick_first,pick_last);
+        
+    }
+    
+    bool stoneGame(vector<int>& nums) 
+    {
+        int sum = 0;
+        for(auto key : nums)
+        {
+            sum = sum + key;
+        }
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        int alice = help(nums,0,nums.size()-1,dp);
+        int bob = sum - alice;
+        
+        if(alice>bob)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 };
