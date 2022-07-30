a9b831b6c79c42d0b7918d15bb697995935400c2
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2022-07-30 18:40:17 +0530

    Time: 37 ms (96.24%), Space: 19.1 MB (65.98%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) 
-    {
-        int n=intervals.size(); //Calculating the size of intervals arrray
-        sort(intervals.begin(),intervals.end()); // sorting array to make it simpler (increasing order of start cordinate)
-        vector<vector<int>> result; //making result array to store maximized/overlapped co-ordinates
-        result.push_back(intervals[0]);//for beginning adding first element
-        for(int i=1;i<n;i++)
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>res;
+        res.push_back(intervals[0]);
+        
+        for(int i=1;i<intervals.size();i++)
         {
-            if(intervals[i][0]<=result[result.size()-1][1])//check if ending of cordinate is bigger than beginning of next cordinate so they can be overlapped
+            if(res[res.size()-1][1]>=intervals[i][0])
             {
-                result[result.size()-1][1] = max(result[result.size()-1][1],intervals[i][1]);//store the minimum beginning and maximum ending to get final overlapped co-ordinates
+                res[res.size()-1][1] = max(res[res.size()-1][1],intervals[i][1]);
             }
             else
             {
-                result.push_back(intervals[i]); // pushback the next co-ordinates if the previous one cannot be overlapped
+                res.push_back(intervals[i]);
             }
         }
-        return result;//return the result
-        
+        return res;
     }
 };
