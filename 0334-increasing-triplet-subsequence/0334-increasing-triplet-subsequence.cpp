f4a3a9781ae5c480739da02394b35aae3524eb99
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2022-10-12 01:50:58 +0530

    Time: 183 ms (12.29%), Space: 61.6 MB (24.07%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int max1 = INT_MIN;
+        int max2 = INT_MIN;
+        int max3 = INT_MIN;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            
+            int curr = nums[i];
+            if(curr > max1)
+            {
+                max1 = curr;
+                
+            }
+            else if(curr > max2 && curr < max1)
+            {
+                max2 = curr;
+                
+            }
+            else if( curr > max3 && curr < max1 && curr < max2)
+            {
+                max3 = curr;
+            }
+            
+            if(max1 != INT_MIN && max2 != INT_MIN && max3 != INT_MIN)
+            {
+                return true;
+            }
+            
+            //cout<<max1<<" "<<max2<<" "<<max3<<endl;
+            
+            
+        }
+        
+        
+        
+        if(max1 == INT_MIN || max2 == INT_MIN || max3 == INT_MIN)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+};
