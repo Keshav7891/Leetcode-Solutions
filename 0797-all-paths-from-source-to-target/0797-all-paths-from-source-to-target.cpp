97ccf1a8aca70f7000a618a83ad4b933989b968c
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2022-10-17 10:45:44 +0530

    Time: 29 ms (47.33%), Space: 16.6 MB (19.39%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>>res;
+    void dfs(int node , int dest , vector<int>path , unordered_map<int,bool>&visited , vector<vector<int>>& adj)
+    {
+        path.push_back(node);
+        visited[node] = true;
+        
+        if(node == dest)
+        {
+            res.push_back(path);
+        }
+        
+        for(auto nbr : adj[node])
+        {
+            if(visited[nbr]==false)
+            {
+                dfs(nbr,dest,path,visited,adj);
+            }
+        }
+        
+        visited[node] = false;
+        path.pop_back();
+        
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int node = 0;
+        int dest = graph.size()-1;
+        unordered_map<int,bool>visited;
+        vector<int>path;
+        dfs(node,dest,path,visited,graph);
+        return res;
+    }
+};
