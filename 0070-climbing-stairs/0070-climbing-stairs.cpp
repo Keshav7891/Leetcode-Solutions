7da4c3334407326bab16b0840e22eaf672b0c91d
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2022-12-12 22:58:10 +0530

    Time: 4 ms (21.07%), Space: 6.2 MB (29.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int help(int n , vector<int>&dp)
+    {
+        if( n == 0)
+        {
+            return 1;
+        }
+        else if(n < 0)
+        {
+            return 0;
+        }
+        
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        
+        int op1 = help(n-1,dp);
+        int op2 = help(n-2,dp);
+        
+        return dp[n] = op1 + op2;
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return help(n,dp);
+    }
+};
