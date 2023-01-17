5d6043e92a85c30d5963e8c6d1fa5667f61b0815
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-01-17 23:28:11 +0530

    Time: 3 ms (55.50%), Space: 6.7 MB (70.02%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int help(int target , vector<int>&nums , vector<int>&dp)
+    {
+        if(target == 0)
+        {
+            return 1;
+        }
+        else if(target < 0)
+        {
+            return 0;
+        }
+        if(dp[target]!=-1)
+        {
+            return dp[target];
+        }
+        int op = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(target-nums[i]>=0)
+            {
+                op = op + help(target - nums[i],nums,dp);
+            }
+        }
+        return dp[target] = op;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        return help(target,nums,dp);
+    }
+};
