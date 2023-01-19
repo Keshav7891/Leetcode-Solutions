5e63fab25335fa562742de8abb16915b3554569c
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-01-19 14:39:12 +0530

    Time: 16 ms (54.78%), Space: 9.6 MB (32.75%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    int dp[51][51][52];
     const int mod = 1e9 + 7;
-    int help(int i , int j , int moves , int m , int n)
+    int help(int i , int j , int moves , int m , int n , vector<vector<vector<int>>>&dp)
     {
-        if(i==m || j==n || i<0 || j<0)  return 1; 
-        if(moves==0)                    return 0; 
-        if(dp[i][j][moves]!=-1)         return dp[i][j][moves];
+        if(moves < 0)
+        {
+            return 0;
+        }
+        if(i>=m || j>=n || i<0 || j<0)
+        {
+            if(moves>=0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        if(dp[i][j][moves]!=-1)
+        {
+            return dp[i][j][moves];
+        }
         int res = 0;
-        res = (res + help(i+1,j,moves-1,m,n))%mod;
-        res = (res + help(i,j+1,moves-1,m,n))%mod;
-        res = (res + help(i-1,j,moves-1,m,n))%mod;
-        res = (res + help(i,j-1,moves-1,m,n))%mod;
+        res = (res + help(i+1,j,moves-1,m,n,dp))%mod;
+        res = (res + help(i,j+1,moves-1,m,n,dp))%mod;
+        res = (res + help(i-1,j,moves-1,m,n,dp))%mod;
+        res = (res + help(i,j-1,moves-1,m,n,dp))%mod;
         
         return dp[i][j][moves] = res%mod;
     }
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        memset(dp,-1,sizeof(dp));
-        return help(startRow,startColumn,maxMove,m,n);
+        vector<vector<vector<int>>>dp(m+1,vector<vector<int>>(n+1,vector<int>(maxMove+1,-1)));
+        return help(startRow,startColumn,maxMove,m,n,dp);
     }
 };
