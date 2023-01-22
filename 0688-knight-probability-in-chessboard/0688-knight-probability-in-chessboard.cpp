4c58554585b893753f7f6c829c01d03e9593db12
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-01-22 18:07:50 +0530

    Time: 30 ms (29.94%), Space: 9.4 MB (19.36%) - LeetHub

---
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    double help(int k , int i , int j , int row , int col , vector<vector<vector<double>>>&dp)
+    {
+        if(i<0 || i>=row || j<0 || j>=col)
+        {
+            return 0;
+        }
+        if(k == 0)
+        {
+            return 1;
+        }
+        if(dp[k][i][j]!=-1)
+        {
+            return dp[k][i][j];
+        }
+        double possibilities;
+        possibilities = help(k-1,i-2,j+1,row,col,dp) + help(k-1,i-1,j+2,row,col,dp) + help(k-1,i+1,j+2,row,col,dp) + help(k-1,i+2,j+1,row,col,dp) + help(k-1,i+2,j-1,row,col,dp) + help(k-1,i+1,j-2,row,col,dp) + help(k-1,i-1,j-2,row,col,dp) + help(k-1,i-2,j-1,row,col,dp);
+        double probablity = possibilities/8;
+        return dp[k][i][j] = probablity;
+    }
+    double knightProbability(int n, int k, int row, int col) {
+        vector<vector<vector<double>>> dp(k + 1, vector<vector<double>> (n + 1, vector<double>(n + 1, -1)));
+        return help(k,row,col,n,n,dp);
+    }
+};
