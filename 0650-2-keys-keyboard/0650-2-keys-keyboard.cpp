2b6d4ed80c85ba78df406d7f0420b57976273bff
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-01-29 21:59:03 +0530

    Time: 83 ms (12.96%), Space: 219.9 MB (8.93%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int help(int len , int copied , int target)
+    int help(int len , int copied , int target , vector<vector<int>>&dp)
     {
         if(len == target)
         {
@@ -10,20 +10,24 @@ public:
         {
             return 1001;
         }
+        if(dp[len][copied]!=-1)
+        {
+            return dp[len][copied];
+        }
         
-        int copy_paste = 2 + help(len + len , len , target);
-        int paste =  1 + help(len + copied , copied , target);
+        int copy_paste = 2 + help(len + len , len , target , dp);
+        int paste =  1 + help(len + copied , copied , target , dp);
         
-        return min({copy_paste , paste});
+        return dp[len][copied] = min({copy_paste , paste});
     }
     int minSteps(int n) 
     {
-        vector<vector<int>>dp;
+        vector<vector<int>>dp(n+1,vector<int>(1000+1,-1));
         if(n==0 || n==1)
         {
             return 0;
         }
-        return 1 + help(1,1,n);
+        return 1 + help(1,1,n,dp);
         
     }
 };
