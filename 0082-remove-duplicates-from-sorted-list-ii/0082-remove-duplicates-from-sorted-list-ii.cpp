8419610de39d6dbdb86ee66515c24e96de79d77d
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-02-15 01:14:43 +0530

    Time: 11 ms (45.30%), Space: 11.2 MB (24.22%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -25,12 +25,13 @@ public:
                     curr = curr->next;
                 }
                 prev->next = curr->next;
+                curr = curr->next;
             }
             else
             {
                 prev = curr;
+                curr = curr->next;
             }
-            curr = curr->next;
         }
         return dummy->next;
     }
