0425e91d6196d76958ba4068cf11f284dac47081
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-07-23 19:48:38 +0530

    Time: 403 ms (5.16%), Space: 67.7 MB (34.15%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,14 +3,29 @@ public:
     int maxSubArray(vector<int>& nums) {
         int res = INT_MIN;
         int currSum = 0;
-        for(auto key : nums){
+        int resStart = -1;
+        int resEnd = -1;
+        int start = -1;
+        int end = -1;
+        for(int i=0;i<nums.size();i++){
+            int key = nums[i];
             if(currSum > 0){
                 currSum = currSum + key;
+                end = i;
             }
             else{
                 currSum = key;
+                start = i;
+                end = i;
             }
-            res = max(res,currSum);
+            if(currSum > res){
+                res = currSum;
+                resStart = start;
+                resEnd = end;
+            }
+        }
+        for(int i=resStart;i<=resEnd;i++){
+            cout<<nums[i]<<" ";
         }
         return res;
     }
