16e23db57eb3aea255c4ad731fad17a8266ce6a6
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-07-23 18:49:42 +0530

    Time: 23 ms (54.36%), Space: 19.6 MB (50.73%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = nums[0];
+        int count = 1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] == majority){
+                count++;
+            }else{
+                count--;
+                if(count == 0){
+                    majority = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        count = 0;
+        for(auto key : nums){
+            if(key == majority){
+                count++;
+            }
+        }
+        if(count > nums.size()/2){
+            return majority;
+        }
+        return -1;
+    }
+};
