1e47b00e483140c385d478a8e305dae31e869b07
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-08-20 03:48:45 +0530

    Time: 3 ms (38.81%), Space: 6.7 MB (23.19%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,42 +1,35 @@
 class Solution {
 public:
-    vector<string> help(string digits , unordered_map<int,string>&m)
-    {
-        if(digits.size() == 0)
-        {
+    vector<string> m = {"","" , "abc" , "def" , "ghi" , "jkl" , "mno" , "pqrs" , "tuv" , "wxyz"};
+    vector<string> help(string digits){
+        if(digits.size() == 0){
             vector<string>res;
             res.push_back("");
             return res;
         }
-        char curr = digits[0] - '0';
-        string s = m[curr];
+        
+        char curr = digits[0];
         string rem = digits.substr(1);
         
-        vector<string>res = help(rem,m);
-        vector<string>newRes;
-        for(auto key : res)
-        {
-            for(auto key1 : s)
-            {
-                newRes.push_back(key1 + key);
+        string alpha = m[curr-'0'];
+        //cout<<curr<<" "<<alpha<<endl;
+        vector<string>prevRes = help(rem);
+        vector<string>res;
+        
+        for(auto key : alpha){
+            for(auto key1 : prevRes){
+                string str = key + key1;
+                res.push_back(str);
             }
         }
         
-        return newRes;
-        
+        return res;
     }
     vector<string> letterCombinations(string digits) {
-        if(digits.size() == 0)  return {};
-       unordered_map<int,string>m;
-        m[2] = "abc";
-        m[3] = "def";
-        m[4] = "ghi";
-        m[5] = "jkl";
-        m[6] = "mno";
-        m[7] = "pqrs";
-        m[8] = "tuv";
-        m[9] = "wxyz";
-        vector<string>res = help(digits,m);
+        if(digits.size() == 0){
+            return {};
+        }
+        vector<string> res= help(digits);
         return res;
     }
 };
