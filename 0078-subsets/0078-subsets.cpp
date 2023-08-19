de6271e32ade6033a046c6c17f7ce4a918d289d6
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-08-20 00:58:44 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (62.10%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-     vector<vector<int>>res;
-    void help(int index , vector<int>&path,vector<int>&nums)
-    {
-        if(index == nums.size())
-        {
+    vector<vector<int>>res;
+    void help(int index , vector<int>&path , vector<int>&nums){
+        if(index == nums.size()){
             res.push_back(path);
             return ;
         }
-        
         path.push_back(nums[index]);
         help(index+1,path,nums);
         path.pop_back();
         help(index+1,path,nums);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+        int index = 0;
         vector<int>path;
-        help(0,path,nums);
+        help(index,path,nums);
         return res;
     }
 };
