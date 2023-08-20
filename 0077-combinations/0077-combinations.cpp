f71e7f440cccab837be4299b53d59bdf5a9e52f6
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-08-20 12:56:29 +0530

    Time: 143 ms (15.26%), Space: 79.3 MB (15.67%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    // _ _
+    vector<vector<int>>res;
+    void help(int n , int index , int k , vector<int>&path){
+        if(k==0){
+            res.push_back(path);
+            return;
+        }
+        if(index > n){
+            return;
+        }
+        
+        for(int i=index;i<=n;i++){
+            path.push_back(i);
+            //cout<<i<<" ";
+            help(n,i+1,k-1,path);
+            path.pop_back();
+        }
+        
+        
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int>path;
+        help(n,1,k,path);
+        return res;
+    }
+};
