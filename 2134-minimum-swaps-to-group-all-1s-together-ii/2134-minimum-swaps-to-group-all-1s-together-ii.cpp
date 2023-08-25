b9cf567db8d39619aa76a4449f004141e1951491
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2023-08-25 15:07:21 +0530

    Time: 71 ms (93.23%), Space: 80.4 MB (84.90%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -9,12 +9,10 @@ public:
         int res = INT_MAX;
         int k = count;
         int zeroCount = 0;
-        int oneCount = 0;
         
         for(int i=0;i<k;i++){
             int curr = nums[i];
             if(curr == 0)   zeroCount++;
-            if(curr == 1)   oneCount++;
         }
         
         res = min(res,zeroCount);
@@ -24,26 +22,21 @@ public:
             int temp = nums[i-k];
         
             if(curr == 0)   zeroCount++;
-            if(curr == 1)   oneCount++;
             
             if(temp == 0)   zeroCount--;
-            if(temp == 1)   oneCount--;
             
             res = min(res,zeroCount);
         }
         
         int leftWindow = k-1;
         int leftWindowZero = 0;
-        int leftWindowOne = 0;
         
         int rightWindow = nums.size()-1;
         int rightWindowZero = 0;
-        int rightWindowOne = 0;
         
         for(int i=0;i<=leftWindow;i++){
             int curr = nums[i];
             if(curr == 0)   leftWindowZero++;
-            if(curr == 1)   leftWindowOne++;
         }
         
         
@@ -51,18 +44,17 @@ public:
         int a = leftWindow;
         int b = nums.size()-1;
         res = min(res,leftWindowZero+rightWindowZero);
-        //cout<<leftWindowZero<<" "<<rightWindowZero<<endl;
+        
         for(int i=1;i<=count;i++){
+            
             int curr1 = nums[leftWindow];
             if(curr1 == 0)  leftWindowZero--;
-            if(curr1 == 1)  leftWindowOne--;
             leftWindow--;
             
             int curr2 = nums[rightWindow];
             if(curr2 == 0)  rightWindowZero++;
-            if(curr2 == 1)  rightWindowOne++;
             rightWindow--;
-            //cout<<leftWindowZero<<" "<<rightWindowZero<<endl;
+            
             res = min(res,leftWindowZero+rightWindowZero);
         }
         
