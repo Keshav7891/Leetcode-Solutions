5b1807ed4650776631e99c9f168441cd96eac5f9
Keshav Allawadi <66181614+Keshav7891@users.noreply.github.com>
2024-05-10 13:52:07 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Swaps to Sort/minimum-swaps-to-sort.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution 
+{
+    // 2 8 5 4
+    // 2 4 5 8
+    
+    public:
+    //Function to find the minimum number of swaps required to sort the array. 
+	int minSwaps(vector<int>& nums) {
+        vector<int> temp = nums;
+        sort(temp.begin(), temp.end());
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            m[nums[i]] = i;
+        }
+
+        int swaps = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != temp[i]) {
+                int correctLocation = m[temp[i]]; // Get the correct location
+                swap(nums[i], nums[correctLocation]); // Swap elements
+                m[nums[i]] = i; // Update map for the swapped elements
+                m[nums[correctLocation]] = correctLocation; // Update map for the swapped elements
+                swaps++; // Increment swap count
+            }
+        }
+
+        return swaps;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < n; i++)
+			cin >> nums[i];
+		Solution obj;
+		int ans = obj.minSwaps(nums);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
